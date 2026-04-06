35703a4d933bd5ef3b2e81d3f17dea2b5fc9a462
KamilaMorais <kamilamorais.ctt@gmail.com>
2026-04-06 09:11:37 -0300

    codigo todo

---
+++ script02_TabelaEstadios.sql
@@ -0,0 +1,28 @@
+﻿BEGIN TRANSACTION;
+CREATE TABLE [TB_ESTADIOS] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] varchar(200) NULL,
+    [Cidade] varchar(200) NULL,
+    [Capacidade] int NOT NULL,
+    CONSTRAINT [PK_TB_ESTADIOS] PRIMARY KEY ([Id])
+);
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Capacidade', N'Cidade', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ESTADIOS]'))
+    SET IDENTITY_INSERT [TB_ESTADIOS] ON;
+INSERT INTO [TB_ESTADIOS] ([Id], [Capacidade], [Cidade], [Nome])
+VALUES (1, 78838, 'Rio de Janeiro', 'Maracanã'),
+(2, 66795, 'São Paulo', 'Morumbi'),
+(3, 43713, 'São Paulo', 'Allianz Parque'),
+(4, 49205, 'São Paulo', 'Arena Corinthians'),
+(5, 61927, 'Belo Horizonte', 'Mineirão'),
+(6, 55662, 'Porto Alegre', 'Arena do Grêmio'),
+(7, 50842, 'Porto Alegre', 'Beira-Rio');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Capacidade', N'Cidade', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ESTADIOS]'))
+    SET IDENTITY_INSERT [TB_ESTADIOS] OFF;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260406111139_MigracaoEstadios', N'10.0.5');
+
+COMMIT;
+GO
+
